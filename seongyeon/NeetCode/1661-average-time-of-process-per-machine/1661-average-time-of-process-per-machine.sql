d7a79379c16d6ef489fd786dd7d5c251149822bc
SEONGYEON KIM <83098550+seongyeon1@users.noreply.github.com>
2024-10-06 10:50:46 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ seongyeon/NeetCode/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,6 @@
+SELECT a1.machine_id, ound(avg(a2.timestamp-a1.timestamp), 3) as processing_time
+FROM Activity a1
+JOIN Activity a2
+ON a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
+and a1.activity_type='start' and a2.activity_type='end'
+GROUP BY a1.machine_id
